6090c7edaef70572d04d681abb9d40245085c702
Higher Stark <higherwashington@gmail.com>
2018-07-06 17:28:44 +0800

    add user data

---
+++ src/test-data/user.js
@@ -0,0 +1,34 @@
+export const User = [
+    {
+        name: 'Judy',
+        password: '123456abc',
+        email: 'judy1964@hotmail.com',
+    },
+    {
+        name: 'Michale',
+        password: '998899bde',
+        email: 'michale1987@foxmail.com',
+    },
+    {
+        name: 'xtq',
+        password: 'xtq@password',
+        email: 'xtq@sjtu.edu.cn',
+    },
+    {
+        name: 'pzy', 
+        password: 'pzy@password',
+        email: 'pzy@sjtu.edu.cn',
+    },
+    {
+        name: 'ybh',
+        password: 'ybh@password',
+        email: 'ybh@sjtu.edu.cn',
+    },
+    {
+        name: 'qpz',
+        password: 'qpz@password',
+        email: 'qpz@sjtu.edu.cn',
+    },
+];
+
+export const CurrentUser = null;

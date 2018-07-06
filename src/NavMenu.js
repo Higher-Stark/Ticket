9bccf7ec103997ca1dc6f3d8af0b4f5fa12e9994
Higher Stark <higherwashington@gmail.com>
2018-07-06 14:51:25 +0800

    Add theme, modify Signup

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -2,83 +2,76 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Home from '@material-ui/icons/Home';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import Star from '@material-ui/icons/Star';
 import Person from '@material-ui/icons/Person';
 import Collections from '@material-ui/icons/Collections';
-import Divider from '@material-ui/core/Divider';
 import Bookmark from '@material-ui/icons/Bookmark';
 import LogoutVariant from 'mdi-material-ui/LogoutVariant';
 import MusicCircle from 'mdi-material-ui/MusicCircle';
 import MovieRoll from 'mdi-material-ui/MovieRoll';
 import Theater from 'mdi-material-ui/Theater';
-import green from '@material-ui/core/colors/green';
-import red from '@material-ui/core/colors/red';
-import yellow from '@material-ui/core/colors/yellow';
-import grey from '@material-ui/core/colors/grey';
-import purple from '@material-ui/core/colors/purple';
 import {NavLink} from 'react-router-dom';
 
 // not login
 export const NavMenuList1 = (
-    <div>
-        <ListItem button onClick={() => {alert("Login")}}>
-            <ListItemIcon><AccountCircle/></ListItemIcon>
-            <ListItemText inset primary='Sign in/up'/>
-        </ListItem>
-        <ListItem button onClick={() => {alert("Market")}}>
-            <ListItemIcon><Home color='primary'/></ListItemIcon>
-            <ListItemText inset primary='Home'/>
-        </ListItem>
-        <ListItem button onClick={() => {alert("Concert")}}>
-            <ListItemIcon><Bookmark color='secondary'/></ListItemIcon>
-            <ListItemText inset primary='Concert'/>
-        </ListItem>
-        <ListItem button onClick={() => {alert("Show")}}>
-            <ListItemIcon><Bookmark color="error"/></ListItemIcon>
-            <ListItemText inset primary='Show'/>
-        </ListItem>
-    </div>
+        <div>
+            <ListItem button component={NavLink} to='/signup'>
+                <ListItemIcon><AccountCircle/></ListItemIcon>
+                <ListItemText inset primary='Sign in/up'/>
+            </ListItem>
+            <Divider/>
+            <ListItem button component={NavLink} to='/'>
+                <ListItemIcon><Home color='primary'/></ListItemIcon>
+                <ListItemText inset primary='Home'/>
+            </ListItem>
+            <ListItem button component={NavLink} to='/concert'>
+                <ListItemIcon><Bookmark color='secondary'/></ListItemIcon>
+                <ListItemText inset primary='Concert'/>
+            </ListItem>
+            <ListItem button component={NavLink} to='/show'>
+                <ListItemIcon><Bookmark color="error"/></ListItemIcon>
+                <ListItemText inset primary='Show'/>
+            </ListItem>
+        </div>
 );
 
 // login
 export const NavMenuList2 = (
     <div>
-        <NavLink to='/signup'>
-            <ListItem button>
-                <ListItemIcon><Person/></ListItemIcon>
-                <ListItemText inset primary='Account'/>
-            </ListItem>
-        </NavLink>
-        <ListItem button>
+        <ListItem button component={NavLink} to='/account'>
+            <ListItemIcon><Person/></ListItemIcon>
+            <ListItemText inset primary='Account'/>
+        </ListItem>
+        <ListItem button component={NavLink} to='/cart'>
             <ListItemIcon><ShoppingCart/></ListItemIcon>
             <ListItemText inset primary='Cart'/>
         </ListItem>
-        <ListItem button>
+        <ListItem button component={NavLink} to='/collection'>
             <ListItemIcon><Collections/></ListItemIcon>
             <ListItemText inset primary='Collection'/>
         </ListItem>
         <Divider/>
-        <ListItem button onClick={() => {alert("Market")}}>
+        <ListItem button component={NavLink} to='/'>
             <ListItemIcon><Home/></ListItemIcon>
             <ListItemText inset primary='Home'/>
         </ListItem>
-        <ListItem button onClick={() => {alert("Concert")}}>
+        <ListItem button component={NavLink} to='/concert'>
             <ListItemIcon><MusicCircle/></ListItemIcon>
             <ListItemText inset primary='Concert'/>
         </ListItem>
-        <ListItem button onClick={() => {alert("Show")}}>
+        <ListItem button component={NavLink} to='/show'>
             <ListItemIcon><Theater/></ListItemIcon>
             <ListItemText inset primary='Show'/>
         </ListItem>
-        <ListItem button>
+        <ListItem button component={NavLink} to='movie'>
             <ListItemIcon><MovieRoll/></ListItemIcon>
             <ListItemText inset primary='Movie'/>
         </ListItem>
         <Divider/>
-        <ListItem button>
+        <ListItem button onClick={() => {alert("logout")}}>
             <ListItemIcon><LogoutVariant/></ListItemIcon>
             <ListItemText inset primary='Logout'/>
         </ListItem>
--- src/Signup.js
+++ src/Signup.js
@@ -1,27 +1,54 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import {withStyles} from '@material-ui/core/styles';
+import {User} from './test-data/user';
 
 const styles = theme => ({
     container: {
         display: 'flex',
         flexWrap: 'wrap',
+        width: 500,
+        alignItems: 'center',
     },
     textField: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
-        width: 360,
-        // alignItems: 'center',
-    },
-    menu: {
-        width: 360,
+        width: '100%',
     },
     verifyImg: {
-        width: '15%',
-        height: '9%',
-    }
+        width: '30%',
+        height: '85%',
+        marginTop: theme.spacing.unit,
+        marginBottom: theme.spacing.unit,
+    },
+    authInput: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: '65%',
+    },
+    button : {
+        width : '50%',
+        textAlign: 'center',
+        paddingLeft: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        marginTop : theme.spacing.unit,
+        alignItems: 'center',
+        margin: 'auto',
+    },
+    content2: {
+        margin: '0 auto',
+        width: 510,
+    },
+    header : {
+        marginBottom: theme.spacing.unit * 2,
+    },
+    reminder: {
+        marginTop: theme.spacing.unit * 2,
+    },
 });
 
 class Signup extends Component{
@@ -39,6 +66,7 @@ class Signup extends Component{
             email: '',
             authCode: '',
             verifyCodes: '',
+            formError: false,
         }
     }
 
@@ -58,20 +86,20 @@ class Signup extends Component{
     };
 
     check_name = () => {
-        let pattern = /^[\w-]{5, 25}$/;
-        return this.state.name.match(pattern);
+        let pattern = /^[\w-]{5,25}$/;
+        return pattern.test(this.state.name);
     };
 
     check_pwd = () => {
-        let pattern = /^[\w-$%#]{6, 25}$/;
-        return this.state.password.match(pattern);
+        let pattern = /^[\w-$%#]{6,25}$/;
+        return pattern.test(this.state.password);
     };
 
     verify = () => {
         return this.state.verifyCodes === this.state.authCode;
     };
 
-    login = () => {
+    signup = () => {
         /*
             fetch ('login', method: {
                 method: 'POST'
@@ -79,55 +107,74 @@ class Signup extends Component{
          */
         if (this.check_name() && this.check_pwd() && this.verify()) {
             alert("Sign up succeed");
+            User.push({
+                name: this.state.name,
+                password: this.state.passive,
+                email: this.state.email,
+            });
+            this.props.history.push('/');
         }
-        else (alert("Sign up failed"));
+        else {
+            console.log(this.check_name());
+            console.log(this.check_pwd());
+            console.log(this.verify());
+            alert("Sign up failed")
+        };
     };
 
     render() {
         const {classes} = this.props;
 
         return (
-            <div>
-                <Typography noWrap>Sign up</Typography>
-                <br/>
+            <div className={classes.content2}>
+                <Typography noWrap className={classes.header} align='center' color='primary' variant='display2'>Sign up</Typography>
                 <form className={classes.container} autoComplete='off'>
-                <TextField placeholder='User Name' id='name' name='name'
-                           value={this.state.name} label='User name'
-                           className={classes.textField}
-                           margin='normal'
-                           required
-                           onChange={this.handleChange('name')}/>
-                    <br/>
-                <TextField placeholder='Password' id='password' name='password'
-                           value={this.state.password} label='Password'
-                           className={classes.textField}
-                           margin='normal'
-                           type='password'
-                           required
-                           onChange={this.handleChange('password')}/>
-                    <br/>
-                <TextField placeholder='Email Address' id='email' name='email'
-                           value={this.state.email} label='Email'
-                           className={classes.textField}
-                           margin='normal'
-                           type='email'
-                           required
-                           onChange={this.handleChange('email')}/>
-                    <br/>
-                <TextField id='authCode' name='authCode'
-                           value={this.state.authCode} label='Verification Code'
-                           className={classes.textField}
-                           margin='normal'
-                           onChange={this.handleChange('authCode')}/>
-                    <img src={this.state.verifyUrl}
+                    <TextField placeholder='User Name' id='name' name='name'
+                               value={this.state.name} label='User name'
+                               className={classes.textField}
+                               margin='normal'
+                               required
+                               onChange={this.handleChange('name')}/>
+                    <TextField placeholder='Password' id='password' name='password'
+                               value={this.state.password} label='Password'
+                               className={classes.textField}
+                               margin='normal'
+                               type='password'
+                               required
+                               onChange={this.handleChange('password')}/>
+                    <TextField placeholder='Email Address' id='email' name='email'
+                               value={this.state.email} label='Email'
+                               className={classes.textField}
+                               margin='normal'
+                               type='email'
+                               required
+                               onChange={this.handleChange('email')}/>
+                    <TextField id='authCode' name='authCode'
+                               value={this.state.authCode} label='Verification Code'
+                               className={classes.authInput}
+                               margin='normal'
+                               onChange={this.handleChange('authCode')}/>
+                    <img src={this.state.verifyUrl} alt="img"
                          onClick={() => this.setState({verifyUrl: this.state.verifyUrl + "3"})}
                          className={classes.verifyImg}/>
-                    <br/>
-                <Button color='primary' onClick={this.login}>Sign Up</Button>
+                    <div className="g-recaptcha" data-sitekey="6LfLkmIUAAAAAO7cmo5x0KgCBtjobIK7M9RzA5Fl"
+                         style={{margin: '0 auto', width: 'auto'}}></div>
+                    <Button color='primary' onClick={this.signup} className={classes.button} variant='contained'>
+                        Sign Up
+                    </Button>
                 </form>
+                <div>
+                    <Typography variant='body1' align='center' noWrap color='secondary' className={classes.reminder}>
+                        Already have an account? <NavLink to='/signin'>Sign in</NavLink>
+                    </Typography>
+                </div>
             </div>
         )
     }
 }
+
+Signup.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Signup);

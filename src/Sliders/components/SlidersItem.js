c8f3c8281205e253565eee35dd46e93e20ecf264
708846441 <708846441@qq.com>
2018-09-08 14:24:06 +0800

    智能推荐，幻灯片链接详细页面，搜索页面无票品时提示

--- src/Sliders/components/SlidersItem.js
+++ src/Sliders/components/SlidersItem.js
@@ -9,7 +9,7 @@ export default class SlidersItem extends Component{
                     className="slider"
                     key={-1}
                 >
-                    <img src={this.props.images[this.props.images.length-1].src} alt=""/>
+                    <img src={this.props.images[this.props.images.length-1].image} alt=""/>
                 </li>
                 {
                     this.props.images.map((item,index)=>(
@@ -17,7 +17,7 @@ export default class SlidersItem extends Component{
                             className="slider"
                             key={index}
                         >
-                            <img src={item.src} alt=""/>
+                            <img src={item.image} alt=""/>
                         </li>
                     ))
                 }
@@ -25,19 +25,19 @@ export default class SlidersItem extends Component{
                     className="slider"
                     key={this.props.images.length}
                 >
-                    <img src={this.props.images[0].src} alt=""/>
+                    <img src={this.props.images[0].image} alt=""/>
                 </li>
                 <li
                     className="slider"
                     key={this.props.images.length+1}
                 >
-                    <img src={this.props.images[1].src} alt=""/>
+                    <img src={this.props.images[1].image} alt=""/>
                 </li>
                 <li
                     className="slider"
                     key={this.props.images.length+2}
                 >
-                    <img src={this.props.images[2].src} alt=""/>
+                    <img src={this.props.images[2].image} alt=""/>
                 </li>
             </ul>
         )
